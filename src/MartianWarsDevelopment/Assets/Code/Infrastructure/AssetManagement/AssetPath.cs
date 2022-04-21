9ad114108f51dd983636dea6e4d08fb0c14d872f
Denis Bezik <denis.bezik@gmail.com>
2022-04-21 20:55:22 +0300

    feat!: new terrain, new rover
    Create new terrain with polybrush plugin, new player's rover implementation

--- src/MartianWarsDevelopment/Assets/Code/Infrastructure/AssetManagement/AssetPath.cs
+++ src/MartianWarsDevelopment/Assets/Code/Infrastructure/AssetManagement/AssetPath.cs
@@ -2,7 +2,7 @@ namespace Code.Infrastructure.AssetManagement
 {
   public static class AssetPath
   {
-    public const string PlayerPath = "Player/Player";
+    public const string PlayerPath = "Player/PlayerRoverMain";
     public const string HudPath = "Hud/HUD";
     public const string ThirdPersonCameraPath = "Cameras/ThirdPersonCamera";
   }
